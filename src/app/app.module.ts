38c9658c820594d61a8549574b43725d391dcbb8
Abel de la Fuente <a.delafuentedelbuey@soprasteria.com>
2021-08-06 21:31:18 +0200

    :sparkless: feat(chore): add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,24 @@ import { UiModule } from '@ui/ui.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [UiModule, BrowserModule, AppRoutingModule, HttpClientModule, ScullyLibModule],
+  imports: [
+    UiModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ScullyLibModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
