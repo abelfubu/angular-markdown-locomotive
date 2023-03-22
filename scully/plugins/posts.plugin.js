d1658a27df20dad935b8c8208afed0f0ab404ec1
Abel de la Fuente <abel.delafuente@feverup.com>
2023-03-22 14:29:53 +0100

    📦️ feat(chore): update scully version

--- scully/plugins/posts.plugin.js
+++ scully/plugins/posts.plugin.js
@@ -1,8 +1,8 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const scully_1 = require('@scullyio/scully');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const scully_1 = require("@scullyio/scully");
 function postIdPlugin(route, config = {}) {
-  return Promise.resolve([{ route: '/blog/1' }]);
+    return Promise.resolve([{ route: '/blog/1' }]);
 }
 const validator = async () => [];
 scully_1.registerPlugin('router', 'postsIds', postIdPlugin, validator);
